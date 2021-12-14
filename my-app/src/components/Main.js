ff6e33b48a44b091bfe229781a57758e9e0969b8
matias acuna <agroang@gmail.com>
2021-12-14 10:48:13 +0900

    styling it's pretty close, added fontawesome dependency

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -2,17 +2,22 @@ import React from "react"
 
 export default function Main () {
   return (
+    <>
     <div className="main-container">
       <h1>Matias Acuna</h1>
       <h4>Full-Stack Web Developer</h4>
       <h6>www.matiasau.me</h6>
       <div className="email-container">
-        <p>need to add the correct html for mail</p>
+        <span>font-awesome icon mail</span>
+        <a href="mailto:agroang@gmail.com">Email</a>
       </div>
+    </div>
+    <div className="second-main-container">
       <h2>About</h2>
       <p>Random information about me</p>
       <h2>Interests</h2>
       <p>Current interests, programming related</p>
     </div>
+    </>
   )
 }
