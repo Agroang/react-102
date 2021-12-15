edea4830bdcafe1c62b21198d7f124070222c038
matias acuna <agroang@gmail.com>
2021-12-15 16:00:09 +0900

    added airbnb logo to what will be a navbar

--- second-app/src/components/Navbar.js
+++ second-app/src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React from "react"
+import airbnb from '../images/airbnb-ar21.svg'
 
 export default function Hero() {
   return (
-    <nav>nav</nav>
+    <nav>
+      <img src={airbnb} alt="airbnb logo"/>
+    </nav>
   )
 }
