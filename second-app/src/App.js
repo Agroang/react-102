111ae7199cdfa6be2722f9f31275607b0fb8cb77
matias acuna <agroang@gmail.com>
2021-12-15 21:22:05 +0900

    added a cool example of how to use JS in JSX as a comment

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -15,3 +15,13 @@ export default function App() {
 }
 // I have used it before, but to use JS inside your JSX you just need to put it
 // between curly braces {}
+
+// Interesting example (it returns the current time in hours)
+// function App() {
+//   const date = new Date()
+//   const hours = date.getHours() % 12
+
+//   return (
+//     <h1>It is currently about {hours} o'clock!</h1>
+//   )
+// }
