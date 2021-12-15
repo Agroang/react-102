6b576dd87fc110c1141638830313abb6c61ca20b
matias acuna <agroang@gmail.com>
2021-12-15 17:20:34 +0900

    styling according to tutorial, some stylying is different as I did it my own

--- second-app/src/components/Card.js
+++ second-app/src/components/Card.js
@@ -1,7 +1,19 @@
 import React from "react"
+import swimmer from '../images/swimmer.png'
+import star from '../images/star.png'
 
 export default function Card() {
   return (
-    <h1>test</h1>
+    <div className="card-container">
+      <img src={swimmer} alt="athelete" />
+      <div className="review-container">
+        <img src={star} alt="star" />
+        <span>6.0&nbsp;</span>
+        <span className="gray">(6) • &nbsp;</span>
+        <span className="gray"> USA</span>
+      </div>
+      <p>Life lessons with Katie Zaferes</p>
+      <p><span className="bold">From $136 </span>/ person</p>
+    </div>
   )
 }
