b2f60a06a1e8a73533a22c4d47b6d13d7715569b
matias acuna <agroang@gmail.com>
2021-12-17 18:53:03 +0900

    added font awesome to the card for location icon, started styling card component

--- chain-food-review/src/components/Card.js
+++ chain-food-review/src/components/Card.js
@@ -1,11 +1,16 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faMapMarkerAlt} />
 
 export default function Card(props) {
   return(
     <div className="card-container">
-
-      {/* // awesomefont the location  */}
+      <div className="location-container">
+      <span>{element}</span>
       <h3>{props.location}</h3>
+      </div>
       <h1>{props.title}</h1>
       <p>{props.reviewDate}</p>
       <p>{props.review}</p>
