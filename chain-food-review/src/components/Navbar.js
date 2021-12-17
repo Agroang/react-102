1b4d9bf2fd4cdfcbdba2b5bc5896a10bd2073cce
matias acuna <agroang@gmail.com>
2021-12-17 18:34:02 +0900

    added styling and fontawesome to the navbar

--- chain-food-review/src/components/Navbar.js
+++ chain-food-review/src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faUtensils } from '@fortawesome/free-solid-svg-icons'
 
-const element = <FontAwesomeIcon icon={faEnvelope} />
+const element = <FontAwesomeIcon icon={faUtensils} />
 
 // add some kind of font awesome or emoticon of a restaurant
 // Style the navbar hehe
 export default function Navbar() {
   return(
     <nav className="navbar">
-      <h1>Matias's Gyudon Restaurants Reviews!</h1><div>{element}</div>
+      <h1>Matias's Gyudon Restaurants Reviews!</h1>
+      <span>{element}</span>
     </nav>
   )
 }
