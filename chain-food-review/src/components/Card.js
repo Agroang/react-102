b14d87f592f51aa9cfe33ac4e24352c8b6f27133
matias acuna <agroang@gmail.com>
2021-12-17 19:09:46 +0900

    card img looks better, need to style the right side with the information

--- chain-food-review/src/components/Card.js
+++ chain-food-review/src/components/Card.js
@@ -7,15 +7,16 @@ const element = <FontAwesomeIcon icon={faMapMarkerAlt} />
 export default function Card(props) {
   return(
     <div className="card-container">
-      <div className="location-container">
-      <span>{element}</span>
-      <h3>{props.location}</h3>
-      </div>
-      <h1>{props.title}</h1>
-      <p>{props.reviewDate}</p>
-      <p>{props.review}</p>
       <img src={props.imageUrl} alt="restaurant"/>
-      <hr />
+      <div className="stats-container">
+        <div className="location-container">
+        <span>{element}</span>
+        <h3>{props.location}</h3>
+        </div>
+        <h1>{props.title}</h1>
+        <p>{props.reviewDate}</p>
+        <p>{props.review}</p>
+      </div>
     </div>
   )
 }
