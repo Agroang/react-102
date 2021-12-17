d0eb7879cd2dbdad27acbd178ceb89b0be4b8011
matias acuna <agroang@gmail.com>
2021-12-17 17:27:57 +0900

    finished making the data that will be used for the card component

--- chain-food-review/src/data.js
+++ chain-food-review/src/data.js
@@ -8,20 +8,20 @@ const dataArray = [
     imageUrl: "https://japanrestaurant.net/wp/wp-content/uploads/2018/05/imgCPkAnK-787x552.jpg"
   },
   {
-    title: "Sukiya",
+    title: "Matsuya",
     location: "Yanokuchi, Inagi",
-    googleMapsUrl: "https://goo.gl/maps/ApEuYSEozNvw23cx6",
-    reviewDate: "12 Jan, 2021",
-    review: "Clean enough compared to other Sukiya restaurants around Tokyo. The menu is absolutely the same, expected from a chain store. Service is really fast.",
-    imageUrl: "https://japanrestaurant.net/wp/wp-content/uploads/2018/05/imgCPkAnK-787x552.jpg"
+    googleMapsUrl: "https://goo.gl/maps/yQv5UyhS32t4kJrf7",
+    reviewDate: "3 Mar, 2021",
+    review: "Less savory compared to Sukiya. Very clean for a chain store and fast, expected from this type of venue. No big differences with Sukiya but personally I feel that the Gyudon from Sukiya taste better.",
+    imageUrl: "https://japanjourneys.jp/wp-content/uploads/2019/12/matsuya-japanese-restaurant-tokyo-matsuya.jpg"
   },
   {
-    title: "Sukiya",
+    title: "Yoshinoya",
     location: "Yanokuchi, Inagi",
-    googleMapsUrl: "https://goo.gl/maps/ApEuYSEozNvw23cx6",
-    reviewDate: "12 Jan, 2021",
-    review: "Clean enough compared to other Sukiya restaurants around Tokyo. The menu is absolutely the same, expected from a chain store. Service is really fast.",
-    imageUrl: "https://japanrestaurant.net/wp/wp-content/uploads/2018/05/imgCPkAnK-787x552.jpg"
+    googleMapsUrl: "https://goo.gl/maps/JUN58WznvdgHM2Sk6",
+    reviewDate: "20 Oct, 2021",
+    review: "Less complexity more volume. It doesn't feel as 'dark' as some other chains. The food tastes just good enough, nothing too fancy. Fast as expected.",
+    imageUrl: "https://old-tokyo.info/wp-content/uploads/2018/07/2018-03-09-11.37.04-1200x756.jpg"
   },
 ]
 
