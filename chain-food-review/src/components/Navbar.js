cc7924237fea0c535b9ac6499dd7ecf8cd1509e4
matias acuna <agroang@gmail.com>
2021-12-17 17:42:50 +0900

    added a few comments on the next steps and modified data

--- chain-food-review/src/components/Navbar.js
+++ chain-food-review/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+// add some kind of font awesome or emoticon of a restaurant
 export default function Navbar() {
   return(
     <nav className="navbar">
--- chain-food-review/src/data.js
+++ chain-food-review/src/data.js
@@ -4,7 +4,7 @@ const dataArray = [
     title: "Sukiya",
     location: "Yanokuchi, Inagi",
     googleMapsUrl: "https://goo.gl/maps/ApEuYSEozNvw23cx6",
-    reviewDate: "12 Jan, 2021",
+    reviewDate: "Jan 12th, 2021",
     review: "Clean enough compared to other Sukiya restaurants around Tokyo. The menu is absolutely the same, expected from a chain store. Service is really fast.",
     imageUrl: "https://japanrestaurant.net/wp/wp-content/uploads/2018/05/imgCPkAnK-787x552.jpg"
   },
@@ -13,7 +13,7 @@ const dataArray = [
     title: "Matsuya",
     location: "Yanokuchi, Inagi",
     googleMapsUrl: "https://goo.gl/maps/yQv5UyhS32t4kJrf7",
-    reviewDate: "3 Mar, 2021",
+    reviewDate: "Mar 3rd, 2021",
     review: "Less savory compared to Sukiya. Very clean for a chain store and fast, expected from this type of venue. No big differences with Sukiya but personally I feel that the Gyudon from Sukiya taste better.",
     imageUrl: "https://japanjourneys.jp/wp-content/uploads/2019/12/matsuya-japanese-restaurant-tokyo-matsuya.jpg"
   },
@@ -22,7 +22,7 @@ const dataArray = [
     title: "Yoshinoya",
     location: "Yanokuchi, Inagi",
     googleMapsUrl: "https://goo.gl/maps/JUN58WznvdgHM2Sk6",
-    reviewDate: "20 Oct, 2021",
+    reviewDate: "Oct 20th, 2021",
     review: "Less complexity more volume. It doesn't feel as 'dark' as some other chains. The food tastes just good enough, nothing too fancy. Fast as expected.",
     imageUrl: "https://old-tokyo.info/wp-content/uploads/2018/07/2018-03-09-11.37.04-1200x756.jpg"
   },
