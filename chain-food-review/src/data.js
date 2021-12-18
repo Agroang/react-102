a89714b74a379b6a1ef889a35dbe65ccb577be4b
matias acuna <agroang@gmail.com>
2021-12-18 09:25:06 +0900

    added rating to the data to make a conditional display with React

--- chain-food-review/src/data.js
+++ chain-food-review/src/data.js
@@ -6,7 +6,8 @@ const dataArray = [
     googleMapsUrl: "https://goo.gl/maps/ApEuYSEozNvw23cx6",
     reviewDate: "Jan 12th, 2021",
     review: "Clean enough compared to other Sukiya restaurants around Tokyo. The menu is absolutely the same, expected from a chain store. Service is really fast.",
-    imageUrl: "https://japanrestaurant.net/wp/wp-content/uploads/2018/05/imgCPkAnK-787x552.jpg"
+    imageUrl: "https://japanrestaurant.net/wp/wp-content/uploads/2018/05/imgCPkAnK-787x552.jpg",
+    rating: 5
   },
   {
     id: 2,
@@ -15,7 +16,8 @@ const dataArray = [
     googleMapsUrl: "https://goo.gl/maps/yQv5UyhS32t4kJrf7",
     reviewDate: "Mar 3rd, 2021",
     review: "Less savory compared to Sukiya. Very clean for a chain store and fast, expected from this type of venue. No big differences with Sukiya but personally I feel that the Gyudon from Sukiya taste better.",
-    imageUrl: "https://japanjourneys.jp/wp-content/uploads/2019/12/matsuya-japanese-restaurant-tokyo-matsuya.jpg"
+    imageUrl: "https://japanjourneys.jp/wp-content/uploads/2019/12/matsuya-japanese-restaurant-tokyo-matsuya.jpg",
+    rating: 3
   },
   {
     id: 3,
@@ -24,7 +26,8 @@ const dataArray = [
     googleMapsUrl: "https://goo.gl/maps/JUN58WznvdgHM2Sk6",
     reviewDate: "Oct 20th, 2021",
     review: "Less complexity more volume. It doesn't feel as 'dark' as some other chains. The food tastes just good enough, nothing too fancy. Fast as expected.",
-    imageUrl: "https://old-tokyo.info/wp-content/uploads/2018/07/2018-03-09-11.37.04-1200x756.jpg"
+    imageUrl: "https://old-tokyo.info/wp-content/uploads/2018/07/2018-03-09-11.37.04-1200x756.jpg",
+    rating: 2
   },
 ]
 
