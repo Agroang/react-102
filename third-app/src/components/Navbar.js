6dc7e5497bdd79725b3097ca0cd4c166bfba8013
matias acuna <agroang@gmail.com>
2021-12-19 10:42:17 +0900

    fixed the img not displaying error

--- third-app/src/components/Navbar.js
+++ third-app/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react"
+import trollFace from "../images/troll_face.png"
 
 export default function Navbar() {
   return(
     <div className="navbar-container">
-      <img />
-      <h1></h1>
-      <h2></h2>
-    </div>>
+      <img src={trollFace} alt="troll face img"/>
+      <h1>test</h1>
+      <h2>test</h2>
+    </div>
   )
 }
