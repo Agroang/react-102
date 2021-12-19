66da6d883662734949243be916face6ea9422ce0
matias acuna <agroang@gmail.com>
2021-12-19 11:01:21 +0900

    styling for header(navbar) it's done but there was a margin that I couldnt target..forced fixed it

--- third-app/src/components/Navbar.js
+++ third-app/src/components/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
   return(
     <div className="navbar-container">
       <img src={trollFace} alt="troll face img"/>
-      <h1>test</h1>
-      <h2>test</h2>
+      <h1>Meme Generator!</h1>
+      <h2>Interactive React Test</h2>
     </div>
   )
 }
