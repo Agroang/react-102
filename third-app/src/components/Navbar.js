30cde240d6dae0cbde2ba0d49f151e9bc46dd6c1
matias acuna <agroang@gmail.com>
2021-12-19 10:34:12 +0900

    created new react app, deleted templates, created new component

---
+++ third-app/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Navbar() {
+  return(
+    <div className="navbar-container">
+      <img />
+      <h1></h1>
+      <h2></h2>
+    </div>>
+  )
+}
