a71499b87a5a70294b89760115a520cfdcb25b71
matias acuna <agroang@gmail.com>
2021-12-21 09:00:17 +0900

    added a nice example of ternary operator use for display on React

--- third-app/src/components/Meme.js
+++ third-app/src/components/Meme.js
@@ -121,3 +121,31 @@ export default function Meme() {
 // }
 
 // The above function is the same as the one above it
+
+// Example of using a ternary operator to flip a boolean and display something
+// different (nice example that mixes some of the stuff that we have been
+// checking)
+
+// export default function App() {
+//   /**
+//    * Challenge:
+//    * - Initialize state for `isGoingOut` as a boolean
+//    * - Make it so clicking the div.state--value flips that
+//    *   boolean value (true -> false, false -> true)
+//    * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+//    */
+//   const [isGoingOut, setIsGoingOut] = React.useState(true)
+
+//   function changeState() {
+//     setIsGoingOut(prevState => !prevState)
+//   }
+
+//   return (
+//     <div className="state">
+//       <h1 className="state--title">Do I feel like going out tonight?</h1>
+//       <div className="state--value" onClick={changeState}>
+//         <h1>{isGoingOut ? "Yes" : "No"}</h1>
+//       </div>
+//     </div>
+//   )
+// }
