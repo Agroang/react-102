1c7ef57169819ff94456eee68aea68d236e6dddc
matias acuna <agroang@gmail.com>
2021-12-24 17:44:57 +0900

    added more examples for textarea and checkbox

--- third-app/src/components/Meme.js
+++ third-app/src/components/Meme.js
@@ -544,3 +544,74 @@ export default function Meme() {
 //     </form>
 //   )
 // }
+
+// HTML inputs have more differences in React:
+// <textarea></textarea> < HTML <textarea /> < React (self closing!)
+
+// Checkboxes are a lot harder, as they don't really have a value in the sense
+// that inputs have, they have a "checked" property that is true or false
+// depending if the box is checked or not, because of that even the handler
+// function must consider this otherwise it simply won't work for the checkbox:
+
+// export default function Form() {
+//   const [formData, setFormData] = React.useState(
+//     {
+//       firstName: "",
+//       lastName: "",
+//       email: "",
+//       comments: "",
+//       isFriendly: true
+//     }
+//   )
+
+//   function handleChange(event) {
+//     const { name, value, type, checked } = event.target
+//     setFormData(prevFormData => {
+//       return {
+//         ...prevFormData,
+//         [name]: type === "checkbox" ? checked : value
+//       }
+//     })
+//   }
+
+//   return (
+//     <form>
+//       <input
+//         type="text"
+//         placeholder="First Name"
+//         onChange={handleChange}
+//         name="firstName"
+//         value={formData.firstName}
+//       />
+//       <input
+//         type="text"
+//         placeholder="Last Name"
+//         onChange={handleChange}
+//         name="lastName"
+//         value={formData.lastName}
+//       />
+//       <input
+//         type="email"
+//         placeholder="Email"
+//         onChange={handleChange}
+//         name="email"
+//         value={formData.email}
+//       />
+//       <textarea
+//         value={formData.comments}
+//         placeholder="Comments"
+//         onChange={handleChange}
+//         name="comments"
+//       />
+//       <input
+//         type="checkbox"
+//         id="isFriendly"
+//         checked={formData.isFriendly}
+//         onChange={handleChange}
+//         name="isFriendly"
+//       />
+//       <label htmlFor="isFriendly">Are you friendly?</label>
+//       <br />
+//     </form>
+//   )
+// }
