8bc3d218a95d0f688f5ad921f051a6688cac4af6
matias acuna <agroang@gmail.com>
2021-12-27 10:50:10 +0900

    added an explanation on async functions inside a useEffect()

--- third-app/src/components/Meme.js
+++ third-app/src/components/Meme.js
@@ -881,3 +881,28 @@ export default function Meme() {
 //     window.removeEventListener("resize", watchWidth)
 //   }
 // }, [])
+
+// NOT always clean up functions are needed, but when there is a memory leak
+// we need to fix that.
+
+// When it comes to async functions inside the useEffect() function, it is
+// possible but unless you have to, then probably you should just stick to
+// using .then
+// Bellow more information about asyn useEffect function:
+
+// useEffect takes a function as its parameter.If that function
+// returns something, it needs to be a cleanup function.Otherwise,
+// it should return nothing.If we make it an async function, it
+// automatically retuns a promise instead of a function or nothing.
+// Therefore, if you want to use async operations inside of useEffect,
+// you need to define the function separately inside of the callback
+// function, as seen below:
+
+// React.useEffect(() => {
+//   async function getMemes() {
+//     const res = await fetch("https://api.imgflip.com/get_memes")
+//     const data = await res.json()
+//     setAllMemes(data.data.memes)
+//   }
+//   getMemes()
+// }, [])
