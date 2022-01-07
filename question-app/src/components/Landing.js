952eaad439771f3449996d0413b93598a3131cf5
matias acuna <agroang@gmail.com>
2022-01-07 19:09:17 +0900

    lading styling looking good, loading button as well, need to keep working on main logic and questions styles

--- question-app/src/components/Landing.js
+++ question-app/src/components/Landing.js
@@ -14,8 +14,8 @@ export default function Landing() {
       {!display &&
         <div className="landing-container">
           <h1 className="title-container">Super Gaming Quiz!</h1>
-          <h2>Amazing description about a lot of useful stuff</h2>
-          <button onClick={toggle}>Start Quiz!</button>
+          <h2 className="description-container">Will you accept the challenge...?</h2>
+          <button className="start-quiz-container" onClick={toggle}>Start Quiz</button>
         </div>
       }
       {display && <Questions />}
