502a08fb8ba21233c620c1908b3d935070782c99
matias acuna <agroang@gmail.com>
2022-01-07 18:22:27 +0900

    added a cool font style for the title

--- question-app/src/components/Landing.js
+++ question-app/src/components/Landing.js
@@ -13,7 +13,7 @@ export default function Landing() {
     <div className="landing-container-test">
       {!display &&
         <div className="landing-container">
-          <h1>Super Quizzzzzzz!!!</h1>
+          <h1 className="title-container">Super Gaming Quiz!</h1>
           <h2>Amazing description about a lot of useful stuff</h2>
           <button onClick={toggle}>Start Quiz!</button>
         </div>
