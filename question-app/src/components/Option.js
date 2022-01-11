0a5ef2e0af02a1c50d2649f9be7c59a84ea2f39b
matias acuna <agroang@gmail.com>
2022-01-11 15:06:12 +0900

    deleted radio button stuff

--- question-app/src/components/Option.js
+++ question-app/src/components/Option.js
@@ -2,54 +2,20 @@ import React from "react"
 import removeMarkdown from "markdown-to-text";
 
 export default function Option(props) {
-  const markdown = props.option;removeMarkdown(markdown);
+  const option = props.option;removeMarkdown(option);
   const [selected, setSelected] = React.useState(props.selected)
-  // state for the radio button, it needs the checked to be a controlled
-  // component and have only 1 button at the same time // not functional
-  // const [radioData, setRadioData] = React.useState({
-  //   value: ""
-  // })
 
   function handleClick() {
     setSelected(oldSelected => !oldSelected)
-    // this is working, props.correctAnswer does represent the correct answer
-    console.log(props.correctAnswer)
   }
-
-  // for radio button //not functional
-  //   function handleChange(event) {
-  //   const { name, value, type, checked } = event.target
-  //     setRadioData(prevRadioData => {
-  //     return { // we don't add a new one as we use value
-  //       ...prevRadioData,
-  //       [name]: type === "checkbox" ? checked : value
-  //     }
-  //   })
-  // }
 
   const selectedColor = selected ? "#38285fe8" : "#835ee0e8"
   const styles = {
     backgroundColor: selectedColor
   }
   return(
-    // radio button version, not functional atm
-    // <div className="button-option-container" onClick={handleClick} style={styles}>
-    //   <input
-    //     type="radio"
-    //     name={markdown}
-    //     value={markdown}
-    //     checked={radioData.value === markdown }
-    //     onChange={handleChange}
-
-    //   />
-    //   <label htmlFor={markdown}>{markdown}</label>
-    // </div>
-    // working version bellow
      <div className="button-option-container" onClick={handleClick} style={styles}>
-      {markdown}
+      {option}
     </div>
   )
 }
-
-// should each optio bet a radio button? how to limit the amount of answers
-// that can be clicked at the same time? has to be a form with styled radio...
