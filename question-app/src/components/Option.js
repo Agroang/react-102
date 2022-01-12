bc560567b7f14d35986a8468ea6a64b0b2a30954
matias acuna <agroang@gmail.com>
2022-01-12 19:16:04 +0900

    was able to fix some of the red errors that I was getting on the console but still only picking up the last component and not all

--- question-app/src/components/Option.js
+++ question-app/src/components/Option.js
@@ -15,13 +15,15 @@ export const Option = React.forwardRef((props, ref) => {
   }
 
   // test function
-  function childFunction () {
-    selected && console.log(`selected and option is ${option}`)
-  }
+  // function childFunction () {
+  //   selected && console.log(`selected and option is ${option}`)
+  // }
 
   // also part of the test
   React.useImperativeHandle(ref, () => ({
-    childFunction
+    childFunction: () => {
+      selected && console.log(`selected and option is ${option}`)
+    }
   }));
 
   const selectedColor = selected ? "#38285fe8" : "#835ee0e8"
--- question-app/src/components/QuestionsTest.js
+++ question-app/src/components/QuestionsTest.js
@@ -70,7 +70,7 @@ export default function QuestionsTest() {
     // return testingElements
   }
 
-
+  // testing useRef
   const onClickFunction = () => {
     myRef.current?.childFunction();
   }
