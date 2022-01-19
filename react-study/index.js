ac9ea8712f9d3d1276c3a1d7eba0d0c1260d6de3
matias acuna <agroang@gmail.com>
2022-01-19 17:19:46 +0900

    added information about ES6 modules (export/import)

--- react-study/index.js
+++ react-study/index.js
@@ -326,3 +326,57 @@ const myUpdatedVehicle = { ...myVehicle, ...updateMyVehicle }
 // The properties that did not match were combined, but the property that did
 // match, color, was overwritten by the last object that was passed,
 // updateMyVehicle. The resulting color is now yellow.
+
+// ES6 Modules -----------------------------------------------------------------
+
+// JavaScript modules allow you to break up your code into separate files.
+// This makes it easier to maintain the code - base.
+// ES Modules rely on the import and export statements.
+
+// Export:
+
+// You can export a function or variable from any file.
+// There are two types of exports: Named and Default.
+
+// Named Exports:
+// You can create named exports two ways.In - line individually, or all at once
+// at the bottom.
+
+// In - line individually:
+
+export const name = "Jesse"
+export const age = "40"
+
+// All at once at the bottom:
+
+const name = "Jesse"
+const age = "40"
+
+export { name, age }
+
+// Default Exports:
+// You can only have one default export in a file.
+
+const message = () => {
+  const name = "Jesse";
+  const age = "40";
+  return name + ' is ' + age + 'years old.';
+};
+
+export default message;
+
+// Import:
+
+// You can import modules into a file in two ways, based on if they are named
+// exports or default exports.
+// Named exports must be destructured using curly braces.Default exports do not.
+
+// Import from named exports
+// Import named exports from the file person.js:
+
+import { name, age } from "./person.js";
+
+// Import from default exports
+// Import a default export from the file message.js:
+
+import message from "./message.js";
