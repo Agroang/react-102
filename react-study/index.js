7a29ae769354fa8584143fbfc8acdb9cc1fa826c
matias acuna <agroang@gmail.com>
2022-01-19 17:08:17 +0900

    added information about ES6 destructuring

--- react-study/index.js
+++ react-study/index.js
@@ -186,3 +186,107 @@ document.getElementById("btn").addEventListener("click", myheader.changeColor);
 // But you CAN:
 // Change the elements of constant array
 // Change the properties of constant object
+
+// ES6 Destructuring -----------------------------------------------------------
+
+// Destructuring makes it easy to extract only what is needed from an object
+// (array, or actual js object)
+
+// Destructuring Arrays:
+
+// Before:
+const vehicles = ['mustang', 'f-150', 'expedition'];
+// old way
+const car = vehicles[0];
+const truck = vehicles[1];
+const suv = vehicles[2];
+
+// With destructuring:
+const vehicles = ['mustang', 'f-150', 'expedition'];
+
+const [car, truck, suv] = vehicles;
+
+// *When destructuring arrays, the order that variables are declared is
+// important.
+
+// You can even leave items out:
+
+const vehicles = ['mustang', 'f-150', 'expedition'];
+
+const [car, , suv] = vehicles; // This picks the first and last item
+
+// Destructuring comes in handy when a function returns an array:
+
+function calculate(a, b) {
+  const add = a + b;
+  const subtract = a - b;
+  const multiply = a * b;
+  const divide = a / b;
+
+  return [add, subtract, multiply, divide];
+}
+
+const [add, subtract, multiply, divide] = calculate(4, 7);
+
+// Destructuring Objects:
+
+// Here is the old way of using an object inside a function:
+
+// Before:
+const vehicleOne = {
+  brand: 'Ford',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021,
+  color: 'red'
+}
+myVehicle(vehicleOne);
+// old way
+function myVehicle(vehicle) {
+  const message = 'My ' + vehicle.type + ' is a ' + vehicle.color + ' ' +
+  vehicle.brand + ' ' + vehicle.model + '.';
+}
+
+// With destructuring:
+const vehicleOne = {
+  brand: 'Ford',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021,
+  color: 'red'
+}
+
+myVehicle(vehicleOne);
+
+function myVehicle({ type, color, brand, model }) {
+  const message = 'My ' + type + ' is a ' + color + ' ' + brand + ' ' +
+  model + '.';
+}
+
+// Doesn't have to be in order like the array destructuring but the names are
+// the keys of the object so naming matters.
+
+// We can even destructure deeply nested objects by referencing the nested
+// object then using a colon and curly braces to again destructure the items
+// needed from the nested object:
+
+const vehicleOne = {
+  brand: 'Ford',
+  model: 'Mustang',
+  type: 'car',
+  year: 2021,
+  color: 'red',
+  registration: {
+    city: 'Houston',
+    state: 'Texas',
+    country: 'USA'
+  }
+}
+
+myVehicle(vehicleOne)
+
+function myVehicle({ model, registration: { state } }) {
+  const message = 'My ' + model + ' is registered in ' + state + '.';
+}
+
+// ES6 Spread Operator ---------------------------------------------------------
