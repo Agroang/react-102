4a0f751c3aee7fd3a9825a68ef7c129e1c8bfb4d
matias acuna <agroang@gmail.com>
2022-01-21 12:15:19 +0900

    up to using the state object from the class component

--- react-study/React-class-components.js
+++ react-study/React-class-components.js
@@ -3,7 +3,7 @@
 // https://reactjs.org/tutorial/tutorial.html
 // https://www.w3schools.com/REACT/DEFAULT.ASP
 
-////////////// https://www.w3schools.com/REACT/react_components.asp ////////////
+//////////////// https://www.w3schools.com/REACT/react_class.asp ///////////////
 
 // React Class Components ------------------------------------------------------
 
@@ -72,3 +72,145 @@ class Car extends React.Component {
 }
 
 // Props
+
+// Props are like function arguments, and you send them into the component as
+// attributes.
+
+// Use an attribute to pass a color to the Car component, and use it in the
+// render() function:
+
+  class Car extends React.Component {
+  render() {
+    return <h2>I am a {this.props.color} Car!</h2>; // "I am a red Car!"
+  }
+}
+
+ReactDOM.render(<Car color="red" />, document.getElementById('root'));
+
+// Props in the Constructor
+
+// If your component has a constructor function, the props should always be
+// passed to the constructor and also to the React.Component via the super()
+// method.
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h2>I am a {this.props.model}!</h2>;
+  }
+}
+
+ReactDOM.render(<Car model="Mustang" />, document.getElementById('root'));
+
+// Components in Components:
+
+// We can refer to components inside other components.
+
+// Use the Car component inside the Garage component:
+
+class Car extends React.Component {
+  render() {
+    return <h2>I am a Car!</h2>;
+  }
+}
+
+class Garage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Who lives in my Garage?</h1>
+        <Car />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Garage />, document.getElementById('root'));
+
+// Components in Files:
+
+// React is all about re - using code, and it can be smart to insert some of
+// your components in separate files.
+
+// To do that, create a new file with a.js file extension and put the code
+// inside it.
+
+// Note that the file must start by importing React(as before), and it has to end
+// with the statement export default Car;
+
+// This is the new file, we named it Car.js:
+
+import React from 'react';
+
+class Car extends React.Component {
+  render() {
+    return <h2>Hi, I am a Car!</h2>;
+  }
+}
+
+export default Car;
+
+// Now we import the Car.js file in the application, and we can use the Car
+// component as if it was created here.
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Car from './Car.js';
+
+ReactDOM.render(<Car />, document.getElementById('root'));
+
+// React Class Component State:
+
+// React Class components have a built -in state object.
+// You might have noticed that we used state earlier in the component
+// constructor section.
+// The state object is where you store property values that belongs to the
+// component.
+// When the state object changes, the component re - renders.
+
+// Creating the state Object:
+
+// The state object is initialized in the constructor:
+
+// Specify the state object in the constructor method:
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { brand: "Ford" };
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Car</h1>
+      </div>
+    );
+  }
+}
+
+// The state object can contain as many properties as you like:
+
+// Specify all the properties your component need:
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Car</h1>
+      </div>
+    );
+  }
+}
+
+// Using the state Object:
