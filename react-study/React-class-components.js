bbe8d80cd7aa32e66de856ff5901599567d2b342
matias acuna <agroang@gmail.com>
2022-01-21 16:14:26 +0900

    added the getSnapshotBeforeUpdate() function information, this one is a little bit more complicated and uses the preState and prevProps

--- react-study/React-class-components.js
+++ react-study/React-class-components.js
@@ -525,3 +525,59 @@ class Header extends React.Component {
 ReactDOM.render(<Header />, document.getElementById('root'));
 
 // ** This once changes from "red" to "blue" as spected **
+
+// render:
+
+// The render() method is of course called when a component gets updated, it has
+// to re-render the HTML to the DOM, with the new changes.
+
+// getSnapshotBeforeUpdate:
+
+// In the getSnapshotBeforeUpdate() method you have access to the props and
+// state before the update, meaning that even after the update, you can check
+// what the values were before the update.
+
+// If the getSnapshotBeforeUpdate() method is present, you should also include
+// the componentDidUpdate() method, otherwise you will get an error.
+
+// The example below might seem complicated, but all it does is this:
+
+// When the component is mounting it is rendered with the favorite color "red".
+// When the component has been mounted, a timer changes the state, and after one
+// second, the favorite color becomes "yellow".
+// This action triggers the update phase, and since this component has a
+// getSnapshotBeforeUpdate() method, this method is executed, and writes a
+// message to the empty DIV1 element.
+// Then the componentDidUpdate() method is executed and writes a message in the
+// empty DIV2 element:
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { favoritecolor: "red" };
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ favoritecolor: "yellow" })
+    }, 1000)
+  }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    document.getElementById("div1").innerHTML =
+      "Before the update, the favorite was " + prevState.favoritecolor;
+  }
+  componentDidUpdate() {
+    document.getElementById("div2").innerHTML =
+      "The updated favorite is " + this.state.favoritecolor;
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+        <div id="div1"></div>
+        <div id="div2"></div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
