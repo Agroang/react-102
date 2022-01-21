0a765cdf9035a5b44f7c9beb5e817b3cba60363f
matias acuna <agroang@gmail.com>
2022-01-21 12:30:49 +0900

    adding the functions called when the component is rendered, did constructor and getDerivedStateFromProps, next is render

--- react-study/React-class-components.js
+++ react-study/React-class-components.js
@@ -294,3 +294,77 @@ class Car extends React.Component {
 // during its three main phases.
 
 // The three phases are: Mounting, Updating, and Unmounting.
+
+// Mounting:
+
+// Mounting means putting elements into the DOM.
+// React has four built -in methods that gets called, in this order, when
+// mounting a component:
+
+constructor()
+getDerivedStateFromProps()
+render()
+componentDidMount()
+
+// The render() method is required and will always be called, the others are
+// optional and will be called if you define them.
+
+// constructor:
+
+// The constructor() method is called before anything else, when the component
+// is initiated, and it is the natural place to set up the initial state and
+// other initial values.
+
+// The constructor() method is called with the props, as arguments, and you
+// should always start by calling the super(props) before anything else, this
+// will initiate the parent's constructor method and allows the component to
+// inherit methods from its parent (React.Component).
+
+// The constructor method is called, by React, every time you make a component:
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { favoritecolor: "red" };
+  }
+  render() {
+    return (
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+    );
+  }
+}
+
+ReactDOM.render(<Header />, document.getElementById('root'));
+
+// getDerivedStateFromProps:
+
+// The getDerivedStateFromProps() method is called right before rendering the
+// element(s) in the DOM.
+// This is the natural place to set the state object based on the initial props.
+// It takes state as an argument, and returns an object with changes to the
+// state.
+
+// The example below starts with the favorite color being "red", but the
+// getDerivedStateFromProps() method updates the favorite color based on the
+// favcol attribute:
+
+// The getDerivedStateFromProps method is called right before the render method:
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { favoritecolor: "red" };
+  }
+  static getDerivedStateFromProps(props, state) {
+    return { favoritecolor: props.favcol };
+  }
+  render() {
+    return (
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+    ); // "My Favorito Color is yellow"
+  }
+}
+
+ReactDOM.render(<Header favcol="yellow" />, document.getElementById('root'));
+
+// render:
