108d92bb9e16c22935861e6900885b8b01957256
matias acuna <agroang@gmail.com>
2022-01-21 12:21:35 +0900

    started the lifecycle of the component, a topic that I wasn't really aware when dealing with function components

--- react-study/React-class-components.js
+++ react-study/React-class-components.js
@@ -214,3 +214,83 @@ class Car extends React.Component {
 }
 
 // Using the state Object:
+
+// Refer to the state object anywhere in the component by using the
+// this.state.propertyname syntax:
+
+// Refer to the state object in the render() method:
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+      </div>
+    );
+  }
+}
+
+// Changing the state Object:
+
+// To change a value in the state object, use the this.setState() method.
+
+// When a value in the state object changes, the component will re-render,
+// meaning that the output will change according to the new value(s).
+
+// Add a button with an onClick event that will change the color property:
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+  changeColor = () => {
+    this.setState({ color: "blue" });
+  }
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color}
+          {this.state.model}
+          from {this.state.year}.
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor} // on click the color property changes
+        >Change color</button>
+      </div>
+    );
+  }
+}
+
+// ** Always use the setState() method to change the state object, it will
+// ensure that the component knows its been updated and calls the render()
+// method (and all the other lifecycle methods). **
+
+// Lifecycle of Components:
+
+// Each component in React has a lifecycle which you can monitor and manipulate
+// during its three main phases.
+
+// The three phases are: Mounting, Updating, and Unmounting.
