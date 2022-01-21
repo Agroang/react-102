a668958afdfe7b4187d36e4cd65f54b36f366f1c
matias acuna <agroang@gmail.com>
2022-01-21 17:24:02 +0900

    started the props file

---
+++ react-study/React-props.js
@@ -0,0 +1,8 @@
+// https://www.freecodecamp.org/news/react-beginners-guide/
+// https://www.freecodecamp.org/news/javascript-skills-you-need-for-react-practical-examples/
+// https://reactjs.org/tutorial/tutorial.html
+// https://www.w3schools.com/REACT/DEFAULT.ASP
+
+//////////////// https://www.w3schools.com/REACT/react_props.asp ///////////////
+
+// React Props -----------------------------------------------------------------
