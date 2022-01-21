551c9a1601db9f17da71b4f61ec681f4e87e79cb
matias acuna <agroang@gmail.com>
2022-01-21 17:41:08 +0900

    added a special note on the React event object, finished information about events

--- react-study/React-events.js
+++ react-study/React-events.js
@@ -59,3 +59,26 @@ function Football() {
 ReactDOM.render(<Football />, document.getElementById('root'));
 
 // React Event Object:
+
+// Event handlers have access to the React event that triggered the function.
+// ** This is important **
+
+// In our example the event is the "click" event.
+
+// Arrow Function: Sending the event object manually:
+
+function Football() {
+  const shoot = (a, b) => {
+    alert(b.type);
+    /*
+    'b' represents the React event that triggered the function,
+    in this case the 'click' event, so alerts 'click'
+    */
+  }
+
+  return (
+    <button onClick={(event) => shoot("Goal!", event)}>Take the shot!</button>
+  );
+}
+
+ReactDOM.render(<Football />, document.getElementById('root'));
