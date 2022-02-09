095b4ec9ae2a41e6872b95bdbc3794785bb0fe82
matias acuna <agroang@gmail.com>
2022-02-09 17:13:29 +0900

    added props to our components, still written in an old style, but to understand what is going on behind scenes

--- react-study-2/adopt-me/src/App.js
+++ react-study-2/adopt-me/src/App.js
@@ -1,29 +1,33 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Luna"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Havanese"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
 };
 
 const App = () => { // Once we use JSX we never write createElement again
   // but it's good to understand whats happening under the hood
-  return React.createElement( "div", {}, [ // it seems the [] is optional, used when passing a bunch of components
+  return React.createElement( "div", {}, [
+    // it seems the [] is optional, used when passing a bunch of components
     // you could pass html attributes here in the {}, same bellow
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "Cat",
+      breed: "Mix"
+    }),
     ]);
 };
-// Same as above, just cleaner
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet),
-//     React.createElement(Pet),
-//     React.createElement(Pet),
-//   ]);
-// };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
