90fb35d75ac94d49546aa0fa21de10ee79ec77b4
matias acuna <agroang@gmail.com>
2022-02-09 17:41:30 +0900

    added config to prettier

--- react-study-2/adopt-me/src/App.js
+++ react-study-2/adopt-me/src/App.js
@@ -6,9 +6,10 @@ const Pet = (props) => {
   ]);
 };
 
-const App = () => { // Once we use JSX we never write createElement again
+const App = () => {
+  // Once we use JSX we never write createElement again
   // but it's good to understand whats happening under the hood
-  return React.createElement( "div", {}, [
+  return React.createElement("div", {}, [
     // it seems the [] is optional, used when passing a bunch of components
     // you could pass html attributes here in the {}, same bellow
     React.createElement("h1", {}, "Adopt Me!"),
@@ -25,9 +26,9 @@ const App = () => { // Once we use JSX we never write createElement again
     React.createElement(Pet, {
       name: "Doink",
       animal: "Cat",
-      breed: "Mix"
+      breed: "Mix",
     }),
-    ]);
+  ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
