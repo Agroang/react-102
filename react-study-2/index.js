6911255e37940fd69b0425ed28e33e779617e477
matias acuna <agroang@gmail.com>
2022-02-09 15:56:34 +0900

    added the resources

---
+++ react-study-2/index.js
@@ -0,0 +1,9 @@
+// Resources:
+// Videos:
+// https://frontendmasters.com/courses/complete-react-v6/
+
+// General Repo:
+// https://github.com/btholt/complete-intro-to-react-v6
+
+// Examples Repo:
+// https://github.com/btholt/citr-v6-project/
