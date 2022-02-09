ab814ead02f82c1c3284294461862c71a400b14a
matias acuna <agroang@gmail.com>
2022-02-09 18:06:15 +0900

    added config for eslint

--- react-study-2/index.js
+++ react-study-2/index.js
@@ -7,3 +7,10 @@
 
 // Examples Repo:
 // https://github.com/btholt/citr-v6-project/
+
+// Website for the course:
+// https://btholt.github.io/complete-intro-to-react-v6/
+
+// ************
+// We are up to:
+// https://frontendmasters.com/courses/complete-react-v6/git/
