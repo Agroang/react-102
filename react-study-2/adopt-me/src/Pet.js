d1765498f1988f587328c714d1ba2a25b199847b
matias acuna <agroang@gmail.com>
2022-02-11 11:03:55 +0900

    commented out old version of Pet and wrote it's JSX version

--- react-study-2/adopt-me/src/Pet.js
+++ react-study-2/adopt-me/src/Pet.js
@@ -1,11 +1,24 @@
 import React from "react"
 
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h1", {}, props.name),
+//     React.createElement("h2", {}, props.animal),
+//     React.createElement("h2", {}, props.breed),
+//   ]);
+// };
+
+
+// This is the same as above, bellow is JSX version, and it transform it into
+// the above
 const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h2>{props.animal}</h2>
+      <h2>{props.breed}</h2>
+    </div>
+  )
+}
 
 export default Pet;
