058801eb95e7aea8a422de237bf06ff75e76fa42
matias acuna <agroang@gmail.com>
2022-02-11 11:12:32 +0900

    changed the app component to use JSX, added a comment about import React with latests React versions

--- react-study-2/adopt-me/src/App.js
+++ react-study-2/adopt-me/src/App.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React from "react"; // With the latests versions of React we really
+// dont have to import React anymore, Babel is intelligent enough to do it for
+// you
 import ReactDOM from "react-dom";
 import Pet from "./Pet"
 
+// const App = () => {
+//   // Once we use JSX we never write createElement again
+//   // but it's good to understand whats happening under the hood
+//   return React.createElement("div", {}, [
+//     // it seems the [] is optional, used when passing a bunch of components
+//     // you could pass html attributes here in the {}, same bellow
+//     React.createElement("h1", {}, "Adopt Me!"),
+//     React.createElement(Pet, {
+//       name: "Luna",
+//       animal: "Dog",
+//       breed: "Havanese",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Pepper",
+//       animal: "Bird",
+//       breed: "Cockatiel",
+//     }),
+//     React.createElement(Pet, {
+//       name: "Doink",
+//       animal: "Cat",
+//       breed: "Mix",
+//     }),
+//   ]);
+// };
+
 const App = () => {
-  // Once we use JSX we never write createElement again
-  // but it's good to understand whats happening under the hood
-  return React.createElement("div", {}, [
-    // it seems the [] is optional, used when passing a bunch of components
-    // you could pass html attributes here in the {}, same bellow
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix",
-    }),
-  ]);
-};
+  return (
+  <div>
+    <h1>Adopt Me!</h1>
+    <Pet
+      name="Luna"
+      animal="Dog"
+      breed="Havanese"
+    />
+    <Pet
+      name="Pepper"
+      animal="Bird"
+      breed="Cockatiel"
+    />
+    <Pet
+      name="Doink"
+      animal="Cat"
+      breed="Mix"
+    />
+  </div>
+  )
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
