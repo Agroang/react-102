5cdfa72ceb54e0fe0df60a6f6d4227646cfbfe20
matias acuna <agroang@gmail.com>
2022-02-13 11:02:27 +0900

    added more options and more hooks, will add useeffect later with an api call

--- react-study-2/adopt-me/src/SearchParams.js
+++ react-study-2/adopt-me/src/SearchParams.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   // you can write a function for onChange if you want to be more verbose:
@@ -11,6 +13,10 @@ const SearchParams = () => {
 
   // NEVER put state inside loops, or if statements, it really messes order up
   // and you can get weird rendering, its a no-no
+  const [animal, updateAnimal] = useState("");
+  const [breed, updateBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
@@ -22,6 +28,41 @@ const SearchParams = () => {
           onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => updateAnimal(e.target.value)}
+            onBlur={(e) => updateAnimal(e.target.value)}
+            // onBlue is for accesibility
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>;
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            // disabled property to disable the dropdown when you don't have any breeds.
+            id="breed"
+            value={breed}
+            onChange={(e) => updateBreed(e.target.value)}
+            onBlur={(e) => updateBreed(e.target.value)}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>;
         <button>Submit</button>
       </form>
     </div>
