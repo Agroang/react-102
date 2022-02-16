59fdd3bb5700e7c1b3e4ba9149ae251d514c5549
matias acuna <agroang@gmail.com>
2022-02-16 09:43:36 +0900

    added general info about React hooks and created file for useState

--- react-study/React-hooks-general.js
+++ react-study/React-hooks-general.js
@@ -6,3 +6,68 @@
 ////////////// https://www.w3schools.com/REACT/react_hooks.asp /////////////////
 
 // React Hooks -----------------------------------------------------------------
+
+// Hooks were added to React in version 16.8.
+// Hooks allow function components to have access to state and other React
+// features.Because of this, class components are generally no longer needed.
+// Although Hooks generally replace class components, there are no plans to
+// remove classes from React.
+
+// What is a Hook?
+
+// Hooks allow us to "hook" into React features such as state and lifecycle
+// methods.
+
+// Example (useState hook):
+
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button
+        type="button"
+        onClick={() => setColor("blue")}
+      >Blue</button>
+      <button
+        type="button"
+        onClick={() => setColor("red")}
+      >Red</button>
+      <button
+        type="button"
+        onClick={() => setColor("pink")}
+      >Pink</button>
+      <button
+        type="button"
+        onClick={() => setColor("green")}
+      >Green</button>
+    </>
+  );
+}
+
+ReactDOM.render(<FavoriteColor />, document.getElementById('root'));
+
+// You MUST import Hooks from react.
+// Above we are using the useState Hook to keep track of the application state.
+// State generally refers to application data or properties that need to be
+// tracked.
+
+// Hook rules:
+
+// - Hooks can only be called inside React function components.
+// - Hooks can only be called at the top level of a component.
+// - Hooks cannot be conditional
+
+// *Hooks will not work in React class components.*
+
+// Custom hooks:
+
+// If you have stateful logic that needs to be reused in several components,
+// you can build your own custom Hooks.
+
+// *useState & useEffect will be 99% of all the hooks you need, so don't bother
+// with custom hooks unless needed*
